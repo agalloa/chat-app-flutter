43937ae383116a7af96e57cf09d7ac834c30b34c
Angelica Gallo <angaar97@gmail.com>
2023-04-28 12:15:42 -0500

    final UI v1

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/her_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(2.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://styles.redditmedia.com/t5_3jytxb/styles/communityIcon_rxz66xlf05s91.jpg'),
+          ),
+        ),
+        title: const Text('My Love'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return (index % 2 == 0)
+                    ? const HerMessageBubble()
+                    : const MyMessageBubble();
+              },
+            ),
+          ),
+          const MessageFieldBox()
+        ]),
+      ),
+    );
+  }
+}
